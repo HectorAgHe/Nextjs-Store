60be3d5fa68c73d935d778b10d40876b9ce750a8
HectorAgHe <aguilarhector440@gmail.com>
2024-04-19 02:03:11 -0600

    19 loading component solution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
 
 import { Roboto } from 'next/font/google'
-
+import { Hero } from "app/components/home/Hero"
+import { Description } from "app/components/home/Description"
 import { Header } from 'app/components/shared/Header'
 import { Footer } from 'app/components/shared/Footer'
 import 'app/sass/globals.sass'
 
 const roboto = Roboto({
-   weight: ["100", "300","500", "700"],
+  weight: ["100", "300", "500", "700"],
   subsets: ["latin"], 
   })
   
-
-
-
 
 export default function RootLayout({
   children,
@@ -25,6 +23,8 @@ export default function RootLayout({
     <html lang="en">
         <body className={roboto.className}>
         <Header />
+        <Hero />
+        <Description />
         {children}
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 
 
-import { Hero } from "app/components/home/Hero"
-import { Description } from "app/components/home/Description"
 import { MainProducts } from "app/components/home/MainProducts"
 
 export default function Home() {
   console.log('Hola home')
   return (
     <main>
-    <Hero />
-    <Description />
-    <MainProducts />
+      <MainProducts />
   </main>
   )
 }

6ca6e26a99e318e16a258fda1061ec6788601b45
HectorAgHe <aguilarhector440@gmail.com>
2024-04-19 02:45:50 -0600

    23 implementando error y pagina not found

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -9,6 +9,7 @@ interface CategoryProps {
 
 export default function Category(props: CategoryProps){
     const {categories} = props.params
+    // throw new Error('Error: boom')
     return(
         <h1>Categoria dinamica: {categories} </h1>
     )
